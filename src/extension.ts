667d5fac718ba2ae33c4a96b338a7ec9d6e27298
guchey <taguchi8shingen@gmail.com>
2023-06-27 12:41:54 +0900

    add file

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let provider = vscode.languages.registerDefinitionProvider('sql', {
+	let provider_ref = vscode.languages.registerDefinitionProvider('sql', {
 		provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
 		  let range = document.getWordRangeAtPosition(position);
 		  let name = document.getText(range);
@@ -10,7 +10,18 @@ export function activate(context: vscode.ExtensionContext) {
 		  });
 		}
 	  });
-	  context.subscriptions.push(provider);
+	context.subscriptions.push(provider_ref);
+
+	let provider_source = vscode.languages.registerDefinitionProvider('sql', {
+		provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
+		  let range = document.getWordRangeAtPosition(position);
+		  let name = document.getText(range);
+		  return vscode.workspace.findFiles('**/' + 'src_' + name + '.yml', null, 1, token).then(result => result[0]).then((uri) => {
+			return new vscode.Location(uri, new vscode.Position(0, 0));
+		  });
+		}
+	  });
+	context.subscriptions.push(provider_source);
 }
 
 // This method is called when your extension is deactivated
