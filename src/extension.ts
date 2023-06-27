6cf113153a556492fff4da359309defcaf4508ca
guchey <taguchi8shingen@gmail.com>
2023-06-27 12:26:40 +0900

    Update script

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,16 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dbt-code-jump" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dbt-code-jump.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from dbt-code-jump!');
-	});
-
-	context.subscriptions.push(disposable);
+	let provider = vscode.languages.registerDefinitionProvider('sql', {
+		provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
+		  let range = document.getWordRangeAtPosition(position);
+		  let name = document.getText(range);
+		  return vscode.workspace.findFiles('**/' + name + '.sql', null, 1, token).then(result => result[0]).then((uri) => {
+			return new vscode.Location(uri, new vscode.Position(0, 0));
+		  });
+		}
+	  });
+	  context.subscriptions.push(provider);
 }
 
 // This method is called when your extension is deactivated
